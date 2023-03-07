6dd9cbba526b33881f91b77a661d71f1c751b20f
Elena Karsten <e.karstenzv@gmail.com>
2023-03-07 12:46:24 +0800

    init

---
+++ Documents/Nethology/ajs-homeworks-master/modules/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  // },
+}
