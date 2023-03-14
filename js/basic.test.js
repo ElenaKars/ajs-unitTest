87e3cd800d70c423b0455c6d7830f8587a8ccfa0
Elena Karsten <e.karstenzv@gmail.com>
2023-03-14 16:55:41 +0800

    first commit

---
+++ js/basic.test.js
@@ -0,0 +1,16 @@
+import getHealthStatus from "./index";
+
+test("health > 50", () => {
+  const player = { name: "Маг", health: 90 };
+  expect(getHealthStatus(player)).toBe("healthy");
+});
+
+test("health from 50 to 15", () => {
+  const player = { name: "Маг", health: 50 };
+  expect(getHealthStatus(player)).toBe("wounded");
+});
+
+test("health < 15", () => {
+  const player = { name: "Маг", health: 10 };
+  expect(getHealthStatus(player)).toBe("critical");
+});
