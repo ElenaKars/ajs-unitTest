33380838d40588a788482e8fc30c603e8c2a648b
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2023-03-20 20:42:49 +0800

    Delete index.js

--- js/index.js
+++
@@ -1,8 +0,0 @@
-export default function getHealthStatus({ name, health }) {
-  if (health > 50) {
-    return "healthy";
-  } if (health >= 15) {
-    return "wounded";
-  }
-  return "critical";
-}
