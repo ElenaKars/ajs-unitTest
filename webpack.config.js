24efe3fd5f2bc71d462bbe5e3a167ceaeb787ab9
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2023-04-21 19:33:19 +0800

    updated after eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   output: {
@@ -39,4 +39,4 @@ module.exports = {
       chunkFilename: '[id].css',
     }),
   ],
-}
+};
